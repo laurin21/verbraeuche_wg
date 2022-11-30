286c8be5eadf32dd9d6362c095c94b626619ac12
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 12:56:27 +0100

    Update Gas.py

--- Gas.py
+++ Gas.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 import time
 import gspread
@@ -29,14 +29,30 @@ gas["Average"] = avg_gas
 gas["Datum"] = pd.to_datetime(gas["Datum"], format = "%d.%m.%Y", errors = "coerce")
 gas_months = gas.groupby(gas.Datum.dt.month)["Gas"].sum()
 
-st.write(gas_months)
-
-jetzt =  datetime.datetime.now()
+jetzt = gas["Datum"][len(gas["Datum"])-1]
 one_month_ago = jetzt - timedelta(days = 30)
 
-st.write(one_month_ago)
+gas_last_month = gas[gas["Datum"] > one_month_ago]
+
+index_list = gas_last_month.index
+
+verbrauch_last_month = gas["Gas"][index_list[-1]] - gas["Gas"][index_list[0]]
+
+costs_last_month =  round(((20.16333333 + verbrauch_last_month *0.1596 * 10) / 100),2)
+
+costs_last_month_pp = round(costs_last_month / 3,2)
+
+
+###
 
 st.title("Gasverbrauch")
+
+st.subheader(f"In den letzten 30 Tagen haben wir für {costs_last_month}€ Gas verbraucht.")
+st.write(f"Das sind {costs_last_month_pp} pro Person.")
+st.write("")
+st.write("Zählerstände der letzten 30 Tage:")
+st.write(gas_last_month[["Datum", "Gas"]])
+
 fig, ax = plt.subplots()
 ax.plot(gas["Datum"], gas["Gas"])
 ax.plot(gas["Datum"], gas["Average"], c = "r")
@@ -45,3 +61,10 @@ ax.spines["top"].set_visible(False)
 ax.tick_params(right = False ,
                 labelbottom = False, bottom = False)
 st.pyplot(fig)
+
+st.write("")
+st.markdown("---")
+st.write("")
+st.write("Gasverbräuche pro Monat:")
+
+st.bar_chart(gas_months)
