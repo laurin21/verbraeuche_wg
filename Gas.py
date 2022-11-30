37c35840807d82ef83b0e001b13f7ffe3ffc50a9
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 12:20:46 +0100

    Update Gas.py

--- Gas.py
+++ Gas.py
@@ -8,29 +8,35 @@ import matplotlib.pyplot as plt
 
 sa = gspread.service_account("service_account.json")
 sh = sa.open("verbraeuche_wg")
+strom = sh.worksheet("Strom")
+strom = pd.DataFrame(strom.get_all_records())
 
-gas = sh.worksheet("Gas")
-gas = pd.DataFrame(gas.get_all_records())
+st.set_page_config(
+    page_title="Strom",
+    page_icon="",
+    menu_items={}
+)
 
 
-st.title("Gasverbrauch")
+
+avg_strom = []
+
+for i in range(len(strom["Strom"])):
+	current_avg = (strom["Strom"][:i].sum()/i)
+	avg_strom.append(current_avg)
+
+strom["Average"] = avg_strom
 
 
-## Gas
-avg_gas = []
 
-for i in range(len(gas["Gas"])):
-	current_avg = (gas["Gas"][:i].sum()/i)
-	avg_gas.append(current_avg)
+st.title("Stromverbrauch")
 
-gas["Average"] = avg_gas
-
-st.subheader("Gas")
 fig, ax = plt.subplots()
-ax.plot(gas["Datum"], gas["Gas"])
-ax.plot(gas["Datum"], gas["Average"], c = "r")
+ax.plot(strom["Datum"], strom["Strom"])
+ax.plot(strom["Datum"], strom["Average"], c = "r")
 ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
 ax.tick_params(right = False ,
                 labelbottom = False, bottom = False)
 st.pyplot(fig)
+
