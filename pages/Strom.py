9f031435e89e8286a4328b095dfe4b0e65f54d68
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 12:08:38 +0100

    Create Strom.py

---
+++ pages/Strom.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import time
+import gspread
+import matplotlib.pyplot as plt
+
+sa = gspread.service_account("service_account.json")
+sh = sa.open("verbraeuche_wg")
+
+strom = sh.worksheet("Strom")
+strom = pd.DataFrame(strom.get_all_records())
+
+st.title("Stromverbrauch")
+
+## Strom
+avg_strom = []
+
+for i in range(len(strom["Strom"])):
+	current_avg = (strom["Strom"][:i].sum()/i)
+	avg_strom.append(current_avg)
+
+strom["Average"] = avg_strom
+
+st.subheader("Strom")
+fig, ax = plt.subplots()
+ax.plot(strom["Datum"], strom["Strom"])
+ax.plot(strom["Datum"], strom["Average"], c = "r")
+ax.spines["right"].set_visible(False)
+ax.spines["top"].set_visible(False)
+ax.tick_params(right = False ,
+                labelbottom = False, bottom = False)
+st.pyplot(fig)
+
