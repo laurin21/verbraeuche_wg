d164d707c9c552987a47acc741afdd53d9ab9d0c
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-11-30 12:09:07 +0100

    Update and rename Verbräuche.py to Gas.py

--- Gas.py
+++ Gas.py
@@ -9,16 +9,12 @@ import matplotlib.pyplot as plt
 sa = gspread.service_account("service_account.json")
 sh = sa.open("verbraeuche_wg")
 
-strom = sh.worksheet("Strom")
-strom = pd.DataFrame(strom.get_all_records())
-
 gas = sh.worksheet("Gas")
 gas = pd.DataFrame(gas.get_all_records())
 
 
-st.title("Vebräuche WG")
+st.title("Gasverbrauch")
 
-gas["Gas"] = gas["Gas"]
 
 ## Gas
 avg_gas = []
@@ -38,23 +34,3 @@ ax.spines["top"].set_visible(False)
 ax.tick_params(right = False ,
                 labelbottom = False, bottom = False)
 st.pyplot(fig)
-
-## Strom
-avg_strom = []
-
-for i in range(len(strom["Strom"])):
-	current_avg = (strom["Strom"][:i].sum()/i)
-	avg_strom.append(current_avg)
-
-strom["Average"] = avg_strom
-
-st.subheader("Strom")
-fig, ax = plt.subplots()
-ax.plot(strom["Datum"], strom["Strom"])
-ax.plot(strom["Datum"], strom["Average"], c = "r")
-ax.spines["right"].set_visible(False)
-ax.spines["top"].set_visible(False)
-ax.tick_params(right = False ,
-                labelbottom = False, bottom = False)
-st.pyplot(fig)
-
