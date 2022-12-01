308eacb44cb3066a774c51bfb0a791a5e537285f
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-12-01 17:46:51 +0100

    Update Strom.py

--- pages/Strom.py
+++ pages/Strom.py
@@ -50,7 +50,7 @@ costs_last_month_pp = round(costs_last_month / 3,2)
 st.title("Stromverbrauch")
 
 st.subheader(f"In den letzten 30 Tagen haben wir für {costs_last_month}€ Strom verbraucht.")
-st.write(f"Das sind {costs_last_month_pp} pro Person.")
+st.write(f"Das sind {costs_last_month_pp}€ pro Person.")
 st.write("")
 st.write("Zählerstände der letzten 30 Tage:")
 st.write(strom_last_month[["Datum", "Strom"]])
