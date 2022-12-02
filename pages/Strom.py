6c27eabf7b1c40ed80e38cd9514ec736a9cd4bc9
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-12-02 17:15:02 +0100

    Update Strom.py

--- pages/Strom.py
+++ pages/Strom.py
@@ -17,8 +17,8 @@ st.set_page_config(
     menu_items={}
 )
 
+rgb_background = "#0f1116"
 
-strom["Strom"] = strom["Strom"].div(100)
 
 avg_strom = []
 
@@ -30,6 +30,8 @@ strom["Average"] = avg_strom
 
 strom["Datum"] = pd.to_datetime(strom["Datum"], format = "%d.%m.%Y", errors = "coerce")
 strom_months = strom.groupby(strom.Datum.dt.month)["Strom"].sum()
+strom_months = pd.DataFrame(strom_months)
+
 
 jetzt = strom["Datum"][len(strom["Datum"])-1]
 one_month_ago = jetzt - timedelta(days = 30)
@@ -40,10 +42,15 @@ index_list = strom_last_month.index
 
 verbrauch_last_month = strom["Strom"][index_list[-1]] - strom["Strom"][index_list[0]]
 
-
-costs_last_month =  round(((8.85 + verbrauch_last_month *39.85) / 100),2)
+costs_last_month =  round(((20.16333333 + verbrauch_last_month *0.1596 * 10) / 100),2)
 
 costs_last_month_pp = round(costs_last_month / 3,2)
+
+month_lst= pd.date_range('2022-04-01', periods = len(strom_months) , freq='1M')-pd.offsets.MonthBegin(1)
+
+month_lst = [date_obj.strftime('%m.%y') for date_obj in month_lst]
+
+strom_months["Month"] = month_lst
 
 ###
 
@@ -59,19 +66,23 @@ fig, ax = plt.subplots()
 ax.plot(strom["Datum"], strom["Strom"])
 ax.plot(strom["Datum"], strom["Average"], c = "r")
 
+
 ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
 ax.spines['bottom'].set_color('white')
 ax.spines['left'].set_color('white')
 ax.tick_params(axis = "both", colors = "white")
-ax.set_xticklabels(labels = strom_months["Month"], rotation=90)
+ax.set_xticklabels(labels = gas_months["Month"], rotation=90)
 fig.patch.set_facecolor(rgb_background)
 ax.set_facecolor(rgb_background)
 
 st.pyplot(fig)
+
 st.write("")
 st.markdown("---")
 st.write("")
 st.write("Stromverbräuche pro Monat:")
 
-st.bar_chart(strom_months)
+st.bar_chart(strom_months["Strom"])
+
+st.write("Hallo Tim :D")
