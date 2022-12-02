bc6912e451fd151f203e8a7950022ef037c79f62
laurin21 <74432244+laurin21@users.noreply.github.com>
2022-12-02 12:40:58 +0100

    Update Gas.py

--- Gas.py
+++ Gas.py
@@ -60,8 +60,8 @@ ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
 ax.tick_params(right = False ,
                 labelbottom = False, bottom = False)
-fig.patch.set_facecolor('#0f1116')
-ax.set_facecolor("0f1116")
+fig.patch.set_facecolor((15,17,22))
+ax.set_facecolor((15,17,22))
 st.pyplot(fig)
 
 st.write("")
